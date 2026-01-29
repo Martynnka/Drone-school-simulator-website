5d6656959eb7ba38213541dc6b8bf0035694a349
Martynas <122054752+Martynnka@users.noreply.github.com>
2026-01-30 01:30:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById("downloadBtn");
+const btns = document.getElementById("downloadBtn1");
 const countEl = document.getElementById("count");
+const ControlsBtn = document.getElementById("ControlsBtn")
 
 
 let downloads = localStorage.getItem("downloads");
@@ -22,3 +24,44 @@ btn.addEventListener("click", () =>
     
     alert("Downloading...");
 });
+
+btns.addEventListener("click", () => 
+    {
+    downloads++;
+    localStorage.setItem("downloads", downloads);
+    countEl.textContent = downloads;
+    
+    alert("Downloading...");
+});
+
+
+btn.addEventListener("mouseenter", () => 
+    {
+        btn.style.background='#10a7ff';
+});
+
+btn.addEventListener("mouseleave", () => 
+    {
+        btn.style.background="#117ED6";
+});
+
+ControlsBtn.addEventListener("mouseenter", () =>
+{
+  ControlsBtn.style.color='#10a7ff';
+});
+
+ControlsBtn.addEventListener("mouseleave", () =>
+{
+  ControlsBtn.style.color='#000000';
+});
+
+btns.addEventListener("mouseenter", () =>
+{
+  btns.style.color='#10a7ff';
+});
+
+btns.addEventListener("mouseleave", () =>
+{
+  btns.style.color='#000000';
+});
+
